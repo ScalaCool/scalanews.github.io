9f8f9f07f53e9e6eb60c119035cc0044a8041b2d
汤俞龙 <541976141@qq.com>
2017-03-31 23:21:46 +0800

    added simple bottom line according to week

--- public/index.js
+++ public/index.js
@@ -4,6 +4,20 @@ $(document).ready(function () {
     if (r.results) {
       console.log('123')
       $('#content .wrap').html(juicer($('#news-tpl').html(), r))
+      addBottomLine()
     }
   })
 })
+
+function addBottomLine(){
+  var times = document.getElementsByTagName("time")
+  var clearfixs = document.getElementsByClassName("clearfix")
+  var weekFlag = -1
+  for (var i = 0; i <= clearfixs.length; i++) {
+    let nowWeekNum = moment(times[i].innerHTML,'YYYY-MM-DD').isoWeeks()
+    if (weekFlag != nowWeekNum) {
+      clearfixs[i].style.borderBottom="2px dotted #ff0000"
+      weekFlag = nowWeekNum
+    }
+  }
+}
